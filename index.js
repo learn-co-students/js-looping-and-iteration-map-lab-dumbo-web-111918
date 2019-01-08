9872761f5c1f36acbbc771e3d58d7d69fca7244b
jriv95 <joel.rivera7995@gmail.com>
2019-01-08 18:40:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(array) {
+  const mappedArray = array.map(function(driver) {
+    return driver.toLowerCase();
+  })
+  return mappedArray;
+}
+
+function nameToAttributes(array) {
+  const keyValue = array.map(makeNameObj)
+  return keyValue;
+}
+
+
+function makeNameObj (fullName) {
+    const splitted = fullName.split(" ");
+    const first = splitted[0]
+    const last = splitted[1]
+    return {firstName: first, lastName: last}
+}
+
+
+function attributesToPhrase(array) {
+    const hometown = array.map(function(obj) {
+      return `${obj.name} is from ${obj.hometown}`
+    })
+    return hometown;
+}
